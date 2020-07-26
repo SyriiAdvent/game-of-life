75bdcdd35faaf2c195c4b808b759cb124c9f20a6
Rudy Goldhaber <rgoldhaber24@gmail.com>
2020-07-26 14:24:22 -0700

    added game rules, drop useless import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ColorManager from './components/themePalette/ColorManager';
 import GameOfLife from './components/gameOfLife/GameOfLife';
 import NavigationBar from './components/navbar/NavigationBar';
