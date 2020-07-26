8074783c4a9d53af30770d43adf01ce8aaf96b5f
Rudy Goldhaber <rgoldhaber24@gmail.com>
2020-07-26 09:20:36 -0700

    Dummy component for each node logic

--- src/components/gameOfLife/Node.tsx
+++ src/components/gameOfLife/Node.tsx
@@ -1,12 +1,32 @@
 import React from 'react'
+import { Theme, createStyles, makeStyles } from "@material-ui/core/styles";
+import { BaseCSSProperties } from "@material-ui/core/styles/withStyles";
 
+interface StyleProps {
+  root: BaseCSSProperties,
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    node: {
+      display: 'flex',
+      width: '20px', 
+      height: '20px', 
+      backgroundColor: 'white', 
+      border: 'solid grey 1px',
+
+      '&:hover': {
+        backgroundColor: '#27A9FF'
+      },
+    },
+  })
+);
 
 // How each grid Node will be represented
 const Node = () => {
+  const cls = useStyles();
   return (
-    <div style={{ width: '20px', height: '20px', backgroundColor: 'cyan' }}>
-      
-    </div>
+    <div className={cls.node} />
   )
 }
 
